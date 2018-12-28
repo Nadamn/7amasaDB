4c0eae24999255978a8ab4f1cc88aef1913ea2f5
Nadamn <nada.mansour01@gmail.com>
2018-12-29 00:36:08 +0200

    gui

--- index.sh
+++ index.sh
@@ -7,62 +7,41 @@ then
 	exit
 fi
 
-echo "Welcome to 7amasaDB" ## change name
-
-
-PS3="main menu select: "
-select choice in "Create New Database" "Delete Existing Database" "Switch to Database" "exit"
+while true
 do
-	PS3="main menu select: "
-	case $REPLY in
+	mainForm=$(yad \
+	--title "7amasa DB" --text "Welcome to 7amasaDB:" \
+	--button="1) Create New Database":1 \
+	--button="2) Delete Existing Database":2 \
+	--button="3) Switch to Database":3 \
+	--button="4) exit":4 \
+	)
+
+
+	choice=$?
+
+	case $choice in
 		1) ./create_db.sh
 		break
 		;;
 
-		2) echo "Enter Database Name"
-		read DBName
+		2) 
+		tmpForm=$(yad --title "7amasa DB" --text "Enter Database Name: " --form --field="name" )
+		DBName=$(echo $tmpForm | awk 'BEGIN {FS="|" } { print $1 }') 
+
 		if [ -d /var/7amasaDB/$DBName ] #change name
 		then
 			./delete_database.sh $DBName
+			continue
 		else
-			echo "Database Not found"
+			yad --title "7amasa DB" --text "Database not found" --button="OK":1
+			continue
 		fi
 		break	
 		;;
 
-		3)
-		echo "Enter Database Name: "
-		read DBName
-		if [ -d /var/7amasaDB/$DBName ]
-		then
-			PS3="database $DBName select: "
-			select choice2 in "Add Table" "Delete Table" "Insert Record" "Update Record" "Delete Record" "Delete Database" "back" "exit" #choices of a specific database
-			do
-				case $REPLY in
-					1) ./create_table.sh $DBName
-					;;
-					2) echo "delete table" ## add its script
-					;;
-					3) ./insert_record.sh $DBName 
-					;;
-					4) echo "update record" ## add its script
-					;;
-					5) echo "delete table" ## add its script
-					;;
-					6) 	./delete_database.sh $DBName
-					;;
-					7) echo "1) Create New Database	      2) Delete Existing Database
-	3) Switch to Database	      4) exit"
-					break
-					;;
-					8) exit
-					;;
-					*) echo Choose a Valid Option! please..
-				esac
-			done
-		else
-			echo "Database Not found"
-		fi    
+		3) ./switch_to_db.sh 
+			continue
 		;;
 
 		4) exit
